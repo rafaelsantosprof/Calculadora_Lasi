e54b8c2f1d0852c97a7b8991b520a43d21670076
rafaelsantosprof <rafaelsantosprof@gmail.com>
2026-06-11 13:51:20 -0300

    Iniciando_projeto

---
+++ calculadora.py
@@ -0,0 +1,5 @@
+# Projeto: calculadora com as quatro operações básicas e duas funções extras:
+# 1ª calculo da distancia percorrida por um projétil;
+# 2ª calculo do determinante de uma matriz.
+# Para o 1º caso, será usada a biblioteca math.
+
