21d79170849a74e222e038562f49994f55ff6ed0
rafaelsantosprof <rafaelsantosprof@gmail.com>
2026-06-11 13:59:14 -0300

    criando_a_primeira_função

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,16 @@
 # Projeto: calculadora com as quatro operações básicas e duas funções extras:
 # 1ª calculo da distancia percorrida por um projétil;
 # 2ª calculo do determinante de uma matriz.
-# Para o 1º caso, será usada a biblioteca math.
 
+# Para o 1º caso, será usada a biblioteca math.
+import math
+
+# Definir cada função:
+def soma():
+    numeroA = float(input("Digite o primeiro número:"))
+    numeroB = float(input("Digite o segundo número:"))
+    res_soma = float(numeroA) + float(numeroB)
+
+    print(f"O resultado da soma é: {res_soma} \n")
+
+soma()
